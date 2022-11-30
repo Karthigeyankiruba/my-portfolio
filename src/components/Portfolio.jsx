a6c6ef9cdf6d70c1d266bc7d4009d56c9ea0af7c
Karthigeyankiruba <karthigeyan0901@gmail.com>
2022-11-30 18:39:50 +0530

    minor refractor

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -54,7 +54,7 @@ const Portfolio = () => {
       className="bg-gradient-to-b from-black
     to-gray-800 w-full text-white md:h-screen pt-12"
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full mt-5">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Portfolio
