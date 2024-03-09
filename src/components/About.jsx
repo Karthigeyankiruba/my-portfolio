b02cdb6ee0f9e28083c257a8f56636567b2b705c
Karthigeyankiruba <karthigeyan0901@gmail.com>
2024-03-09 13:41:28 +0530

    changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,33 +2,55 @@ import React from "react";
 
 const About = () => {
   return (
-    <div name='about' className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
         </div>
+        <div className=" text-zinc-300">
+          <p className="text-xl mt-10">
+            Greetings! I'm Karthigeyan, a dedicated Frontend Developer with a
+            year of hands-on experience. My specialization includes crafting
+            seamless user interfaces using technologies such as ReactJS, NextJS,
+            Material UI, Bootstrap, Tailwind CSS, Redux, and other related
+            libraries.
+          </p>
 
-        <p className="text-lg mt-20">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-          odit provident beatae quasi corporis quod laudantium veniam. Culpa
-          asperiores eius similique aliquid, maiores nam hic sint error animi
-          quod eveniet neque nihil nulla cumque quis at maxime dignissimos
-          doloribus aperiam, accusantium assumenda sed! Aliquam assumenda id
-          nihil, quis eveniet molestias.
-        </p>
+          <br />
 
-        <br />
-
-        <p className="text-xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-          odit provident beatae quasi corporis quod laudantium veniam. Culpa
-          asperiores eius similique aliquid, maiores nam hic sint error animi
-          quod eveniet neque nihil nulla cumque quis at maxime dignissimos
-          doloribus aperiam, accusantium assumenda sed! Aliquam assumenda id
-          nihil, quis eveniet molestias.
-        </p>
-
-
+          <p className="text-xl">
+            In my professional journey, I've been actively involved in
+            developing UI solutions for diverse SaaS products. I thrive in
+            solving complex problems and have successfully contributed to
+            projects dealing with significant challenges, including the
+            integration of complex and big data sets into user interfaces.
+          </p>
+          <br />
+          <p className="text-xl">
+            My approach involves not just creating visually impressive
+            interfaces but ensuring seamless integration with Restful APIs. I
+            take pride in my ability to deliver interactive and user-centric
+            designs.
+          </p>
+          <br />
+          <p className="text-xl">
+            I am honored to have been recognized with the Employee of the Month
+            award for my dedication and hard work on projects. Now, I am
+            actively seeking new career opportunities that will not only propel
+            my professional growth but also allow me to enhance my skills
+            further.
+          </p>
+          <br />
+          <p className="text-xl">
+            Let's collaborate on innovative projects and create impactful user
+            experiences together!
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,37 +1,40 @@
 import React from "react";
 import Profile from "../assets/portfolio/profile3.png";
 import { MdKeyboardArrowRight } from "react-icons/md";
-import {Link} from 'react-scroll'
-
+import { Link } from "react-scroll";
 
 const Home = () => {
   let style = {
-   
-   backgroundColor : "royalblue"
+    backgroundColor: "royalblue",
   };
   return (
     <div
       name="home"
       className="h-screen  w-full bg-gradient-to-b from-black via-black to-gray-800 text-white"
     >
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+      <div className="max-w-screen-xl mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold textwhite">
-            I'm a <font className="text-orange-400">Full Stack Developer </font>
+            I'm a <font className="text-orange-400">Frontend Developer </font>
           </h2>
-          <p className="text-gray-500 py-4 max-w-md">
+          <p className="text-gray-400 py-4 max-w-md text-lg">
             I have 1 years of experience building and desgining software.
             Currently, I love to work on web application using technologies like
-            React, Tailwind, Node JS and GraphQL.
+            React Js, Next Js, Material UI, Tailwind CSS, Bootstrap.
           </p>
 
           <div>
-            <Link to="portfolio" smooth duration={700} className="group text-white w-fit px-6 py-3 my-2 flex 
+            <Link
+              to="portfolio"
+              smooth
+              duration={700}
+              className="group text-white w-fit px-6 py-3 my-2 flex 
             items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500
-            cursor-pointer font-normal">
+            cursor-pointer font-semibold"
+            >
               Portfolio
               <span className="group-hover:rotate-90 duration-300">
-                <MdKeyboardArrowRight size={25} className="ml-1"/>
+                <MdKeyboardArrowRight size={25} className="ml-1" />
               </span>
             </Link>
           </div>
@@ -39,7 +42,7 @@ const Home = () => {
 
         <div>
           <img
-          style={style}
+            style={style}
             src={Profile}
             alt="my profile"
             className="rounded-2xl mx-auto w-2/3 md:w-full"
